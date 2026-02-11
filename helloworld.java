d599b6c35262397c8feba1d89aba9a0da3ffde17
vb2099-coder <vb2099@srmist.edu.in>
2026-02-12 02:28:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,14 @@
-public class HelloWorld{
- public static void main(String args[]){
-     System.out.println("Welcome to Classroom");
+public class HelloWorld {
+    public static void main(String args[]) {
+
+        System.out.println(" *****     *****     ******     *****  ");
+        System.out.println("*     *   *     *    *     *   *     * ");
+        System.out.println("*     *   *     *    *     *   *       ");
+        System.out.println("*     *   *     *    ******     *****  ");
+        System.out.println("*     *   *     *    *               * ");
+        System.out.println("*     *   *     *    *         *     * ");
+        System.out.println(" *****     *****     *          *****  ");
+
     }
 }
+
