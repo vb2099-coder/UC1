e356b82e7535eef8d56de47cecfafab8a9383fd6
Haasini <babyhaasini321@gmail.com>
2026-02-12 02:23:55 +0530

    Added UC2 OOPS Banner

---
+++ UC2.java
@@ -0,0 +1,13 @@
+public class UC2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *               *");
+        System.out.println("*     *  *     *  *         *     *");
+        System.out.println(" *****    *****   *          ***** ");
+
+    }
+}
