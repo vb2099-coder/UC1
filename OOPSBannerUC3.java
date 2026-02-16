20279a179ac1753a8dcf8c0dff1e8a9ca70a1254
Haasini <babyhaasini321@gmail.com>
2026-02-16 11:11:43 +0530

    UC3 - Refactored OOPS Banner using String.join()

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,40 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(String.join(" ",
+                " OOO  ",
+                " OOO  ",
+                " PPPP ",
+                " SSSS "));
+
+        // Line 2
+        System.out.println(String.join(" ",
+                "O   O ",
+                "O   O ",
+                "P   P ",
+                "S     "));
+
+        // Line 3
+        System.out.println(String.join(" ",
+                "O   O ",
+                "O   O ",
+                "PPPP  ",
+                " SSS  "));
+
+        // Line 4
+        System.out.println(String.join(" ",
+                "O   O ",
+                "O   O ",
+                "P     ",
+                "    S "));
+
+        // Line 5
+        System.out.println(String.join(" ",
+                " OOO  ",
+                " OOO  ",
+                "P     ",
+                "SSSS  "));
+    }
+}
